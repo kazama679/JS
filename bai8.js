eba5c564a73860428ec005084e82ab6321065949
kazama679 <147390572+kazama679@users.noreply.github.com>
2024-01-11 14:36:40 +0700

    Add files via upload

---
+++ bai8.js
@@ -0,0 +1,7 @@
+const PI = 3.14159;
+let radius = prompt("bán kính?");
+radius = Number(radius);
+let circumference = 2 * PI * radius;
+let area = PI * radius * radius;
+console.log(circumference);
+console.log(area);
